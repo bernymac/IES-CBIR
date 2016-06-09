0bfb948ad4c6ed5cff63e91c4eedfec16da9437a
Bernardo Ferreira <bernardolferreira@gmail.com>
2016-06-09 18:27:15 +0100

    Updated copyright notices.

--- projects/Owner/src/main/java/NovaSYS/IES_CBIR/utils/OPE/BigFunctions.java
+++ projects/Owner/src/main/java/NovaSYS/IES_CBIR/utils/OPE/BigFunctions.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2015 João Rodrigues
+ *    Copyright 2015 João Miguel Cardia Melro Rodrigues
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
--- projects/Owner/src/main/java/NovaSYS/IES_CBIR/utils/OPE/OPEtest.java
+++ projects/Owner/src/main/java/NovaSYS/IES_CBIR/utils/OPE/OPEtest.java
@@ -1,6 +1,6 @@
 /**
- *    Copyright 2015 João Rodrigues
-
+*    Copyright 2015 João Miguel Cardia Melro Rodrigues
+ 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
